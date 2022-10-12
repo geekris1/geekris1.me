8147003a70684c4f20fccfab7c4a5dfab13b7ff2
geekris1 <200949970@qq.com>
2022-10-12 22:36:47 +0800

    feat:添加百度统计

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Nav from "@/component/Nav";
 import "./App.css";
 import routes from "~react-pages";
 import { useRoutes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
